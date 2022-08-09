08448b0cdab7e97993f4b6ff59bd80eead47bdfa
Kunal Kumar <kunal.kumar@progcap.com>
2022-08-09 13:02:10 +0530

    change the text on onClick

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,77 @@
 import 'package:flutter/material.dart';
 
-void main(){
-  runApp(const MaterialApp(
-    title: "Awesome App",
-    home: MyApp(),
-  ));
+const Color darkBlue = Color.fromARGB(255, 18, 32, 47);
+
+void main() {
+  runApp(const MyApp());
 }
 
-
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  void initState(){
+    super.initState();
+  }
+
   
-  Widget build(BuildContext context){
-    return Scaffold(
-      appBar: AppBar(title: const Text("My App")),
-      body: const Center(
-        child: Text("Hi Kunal"),
-      )
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData.dark().copyWith(
+        scaffoldBackgroundColor: darkBlue,
+      ),
+      debugShowCheckedModeBanner: false,
+      home: const Scaffold(
+        body: Center(
+          child: MyWidget(),
+        ),
+      ),
     );
   }
-} 
+}
+
+class MyWidget extends StatefulWidget {
+  const MyWidget({Key? key}) : super(key: key);
+
+  @override
+  State<MyWidget> createState() => _MyWidgetState();
+}
+
+class _MyWidgetState extends State<MyWidget> {
+  String _name = 'Kunal';
+  void _intro(){
+    setState((){
+      _name = "Welcome to Flutter!";
+    });
+  }
+  
+  
+  @override
+  void initState(){
+    super.initState();
+  }
+  
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        Text(
+      'Hello, $_name',
+      style: Theme.of(context).textTheme.headline4,
+    ),
+        ElevatedButton(
+          onPressed: _intro,
+          child: const Text("Click me")
+        )
+      ]
+    );
+  }
+}
